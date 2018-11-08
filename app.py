9dbe66cbb8ae60e78ba17bc419ca005930e37415
aboud20-meet <aboud20@meet.mit.edu>
2018-11-08 18:47:57 +0200

    flak

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from database import get_all_cats
+from database import get_all_cats, create_cat, get_cat
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
@@ -9,6 +9,19 @@ app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
 def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
+
+@app.route('/cats/<int:id>')
+def cat_profile(id):
+    # TODO: read cat  by id
+    cat=get_cat(id)
+    return render_template(
+        'cat.html', cat=cat)
+
+@app.route('/add_cat')
+def new_cat():
+    return render_template(
+        'add.html')
+
 
 if __name__ == '__main__':
    app.run(debug = True)
